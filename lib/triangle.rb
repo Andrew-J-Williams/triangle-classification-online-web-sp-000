a71371f4eb232ea3cf2895d35eb80ac92fd2e111
Andrew Williams <1williamsaj@gmail.com>
2019-10-13 22:22:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,6 +18,7 @@ class Triangle
     elsif (side_one <= 0) || (side_two <= 0) || (side_three <= 0)
       begin
         raise TriangleError
+        emd
     elsif (side_one + side_two) > side_three || (side_one + side_three) > side_two || (side_two + side_three) > side_one
         raise TriangleError
     end

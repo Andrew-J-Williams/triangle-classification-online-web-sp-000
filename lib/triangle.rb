85c1253fbd216f3bce5653b151b5569426541c97
Andrew Williams <1williamsaj@gmail.com>
2019-10-13 22:26:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
 
   def kind
-    if (side_one * side_two * side_three) == 0 || 
+    if (side_one * side_two * side_three) == 0 || (side_one + side_two) <= side_three || (side_one + side_three) <= side_two ||
   end
 
   class TriangleError < StandardError

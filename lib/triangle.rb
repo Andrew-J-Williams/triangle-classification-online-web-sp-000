2676bee28c30fd5d5924d2bfa0f0999b1b57fc0f
Andrew Williams <1williamsaj@gmail.com>
2019-10-13 22:14:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,14 +16,19 @@ class Triangle
     elsif side_one != side_two && side_two != side_three
       :scalene
     elsif side_one <= 0 || side_two <= 0 || side_three <= 0
+      begin
         raise TriangleError
+        rescue TriangleError => error
+        puts error.message
     elsif side_one + side_two > side_three || side_one + side_three > side_two || side_two + side_three > side_one
       raise TriangleError
     end
   end
 
   class TriangleError < StandardError
-
+    def message
+      "This is not a triangle"
+    end
   end
 
 end
